8215151e50ef4125b62579e0da4d8174a530503f
Matheus Proenca <matheusproencaz@gmail.com>
2022-08-21 11:10:15 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpEvent, HttpRequest, HttpInterceptor, HttpHandler, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService,
+                public alertCtrl: AlertController) {
         
     }
 
@@ -31,7 +33,12 @@ export class ErrorInterceptor implements HttpInterceptor {
                 case 403:
                     this.handle403();
                     break;
-                
+                case 401:
+                    this.handle401();
+                    break;
+                default:
+                    this.handleDefaultError(errorObj);
+                    break;
             }
 
             return Observable.throw(errorObj);
@@ -41,6 +48,34 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: "Error 401: Falha de autenticação",
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: `Erro ${errorObj.status}: ${errorObj.error}`,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 } 
 
