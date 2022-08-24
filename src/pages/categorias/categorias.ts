93df43991144db8090b84a876ac907929afe1d51
Matheus Proenca <matheusproencaz@gmail.com>
2022-08-24 20:39:59 -0300

    Carregando produtos de uma dada categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -29,8 +29,8 @@ export class CategoriasPage {
       error => {});
   }
 
-  showProdutos() {
-    this.navCtrl.push('ProdutosPage');
+  showProdutos(categoria_id) {
+    this.navCtrl.push('ProdutosPage', {categoria_id: categoria_id});
   }
 
 }
