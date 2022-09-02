ca45d59abd7052e41228e2312223f959bcd9549c
Matheus Proenca <matheusproencaz@gmail.com>
2022-09-02 09:31:56 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -9,12 +9,24 @@ export class ProdutoService {
 
     constructor(public http: HttpClient) {}
 
-    findByCategoria(categoria_id: string){
-        return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    findById(produto_id: string){
+        return this.http.get<ProdutoDTO>
+            (`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
     }
+    
+    findByCategoria(categoria_id: string){
+        return this.http.get<ProdutoDTO[]>
+            (`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
 
     getSmallImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url, {responseType: 'blob'});
+    }
+
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
         return this.http.get(url, {responseType: 'blob'});
     }
 }
