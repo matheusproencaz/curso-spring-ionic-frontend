420a3a55f9707372a9298ec1634670a52d8fa8a5
Matheus Proenca <matheusproencaz@gmail.com>
2022-09-14 19:30:53 -0300

    Criando pagina de carrinho de compras

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
@@ -15,7 +16,9 @@ export class ProdutoDetailPage {
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              public produtoService: ProdutoService
+              public produtoService: ProdutoService,
+              public cartService: CartService,
+
     ) {
   }
 
@@ -37,6 +40,11 @@ export class ProdutoDetailPage {
       },
       error => {})
 
+  }
+
+  addToCart(produto: ProdutoDTO) {
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
+import { Cart } from "../models/cart";
 import { LocalUser } from "../models/local_user";
 
 @Injectable()
@@ -7,20 +8,43 @@ export class StorageService {
 
     getLocalUser() : LocalUser {
         let usr = localStorage.getItem(STORAGE_KEYS.localUser);
-
-        if (usr == null) {
-            return null
-        } else {
-            return JSON.parse(usr); //LocalStorage só guarda valores em string;
-
-        }
+        // if (usr == null) {
+        //     return null
+        // } else {
+        //     return JSON.parse(usr); //LocalStorage só guarda valores em string;
+        // }
+        return usr = null ? null : JSON.parse(usr);
     }
 
-    setLocalUser(user: LocalUser) {
-        if(user == null){
-            localStorage.removeItem(STORAGE_KEYS.localUser);
-        } else {
-            localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(user));
-        }
+    setLocalUser(user: LocalUser): void {
+        // if(user == null){
+        //     localStorage.removeItem(STORAGE_KEYS.localUser);
+        // } else {
+        //     localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(user));
+        // }
+        user == null
+            ? localStorage.removeItem(STORAGE_KEYS.localUser)
+            : localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(user));
+    }
+
+    getCart() : Cart {
+        let str = localStorage.getItem(STORAGE_KEYS.cart);
+        // if (str != null){
+        //     return JSON.parse(str);
+        // } else {
+        //     return null;
+        // }
+        return str != null ? JSON.parse(str) : null;
+    }
+
+    setCart(obj: Cart){
+        // if(obj != null){
+        //     localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
+        // } else {
+        //     localStorage.removeItem(STORAGE_KEYS.cart);
+        // }
+        obj != null
+        ? localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj))
+        : localStorage.removeItem(STORAGE_KEYS.cart);
     }
 }
